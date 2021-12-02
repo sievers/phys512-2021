f335e297dbc0232a47503a64efb4da72509b03f3
JLS <jonathan.sievers@gmail.com>
2021-12-02 10:20:16 -0500

    added a self-compressing, fully worked huffman code

--- compression/huffman_fromfile.py
+++ compression/huffman_fromfile.py
@@ -16,6 +16,18 @@ class node:
         else:
             self.left.print(cur+'0',mydict=mydict)
             self.right.print(cur+'1',mydict=mydict)    
+    def to_dict(self,cur='',mydict=None):
+        if mydict is None:
+            mydict={}
+            self.to_dict(mydict=mydict)
+            return mydict
+        if (self.left is None)&(self.right is None):
+            #print(self.name,' ',cur)
+            if not(mydict is None):
+                mydict[self.name]=cur
+        else:
+            self.left.to_dict(cur+'0',mydict=mydict)
+            self.right.to_dict(cur+'1',mydict=mydict)    
     def get_length(self,depth=0):
         if (self.left is None)&(self.right is None):
             #s=-self.prob*np.log2(self.prob)
@@ -67,38 +79,68 @@ def build_tree(names,probs):
 
 def decode_message(encoded,tree):
     message=''
-    while len(encoded)>0:
+    #while len(encoded)>0:
+    while 1:
         tmp,encoded=tree.decode_token(encoded)
-        message=message+tmp
-    return message
+        if tmp=='eom':
+            return message
+        else:
+            message=message+tmp
+    return None
 def encode_message(message,mydict):
     encoded=''
     for i in range(len(message)):
         encoded=encoded+mydict[message[i]]
     return encoded
 
+def make_ptable(dat):
+    mydict={}
+    for i in range(len(dat)):
+        if dat[i] in mydict.keys():
+            mydict[dat[i]]=mydict[dat[i]]+1
+        else:
+            mydict[dat[i]]=1
+    mydict['eom']=1
+    names=[key for key in mydict.keys()]
+    probs=[mydict[key] for key in mydict.keys()]
+    return names,probs
 
-#names=['a','b','c']
-#probs=[0.4,0.25,0.35]
-#names=['a','c','e','d','b']
-#probs=[7,6,5,2,1]
-names=['a','b','c','d','e','f','g']
-probs=[1,1,2,4,8,8,8]
+def encoded_to_arr(encoded):
+    tmp=np.empty(1,dtype='int')
+    nbit=tmp.nbytes*8
+    remainder=len(encoded)%nbit
+    if remainder>0:
+        encoded=encoded+'0'*(nbit-remainder)
+    nword=len(encoded)//nbit
+    myarr=np.empty(nword,dtype='uint')
+    for i in range(nword):
+        i1=i*nbit
+        i2=(i+1)*nbit
+        tmp=encoded[i1:i2]
+        try:
+            #myarr[i]=int(encoded[i1:i2],2)            
+            myarr[i]=int(tmp,2)
+        except:
+            print('failed had len ',len(tmp),' with message ',tmp)
+    return myarr
+
+file=open('huffman_fromfile.py')
+dat=file.read()
+file.close()
+
+names,probs=make_ptable(dat)
 tree=build_tree(names,probs)
-mydict={}
-tree.print(mydict=mydict)
-
 
 print('average bits per character: ',tree.get_length())
 pp=np.asarray(probs,dtype='float')
 pp=pp/pp.sum()
 print('ideal would have been ',-np.sum(pp*np.log2(pp)))
 
-message='cede'
-encoded=encode_message(message,mydict)
-#encoded=''
-#for i in range(len(message)):
-#    encoded=encoded+mydict[message[i]]
-print(message,' when encoded is ',encoded)
+mydict=tree.to_dict()
+encoded=encode_message(dat,mydict)+mydict['eom']
+print('we compressed to ',len(encoded)/8,' bytes')
+
 to_check=decode_message(encoded,tree)
-print('when we decode it we get ',to_check)
+myarr=encoded_to_arr(encoded)
+f=open('myself.compressed','w')
+myarr.tofile(f)
